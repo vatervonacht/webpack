0249f452006160a624b50e29d2f4caf4372d1994
Tim Rozmajzl <tim.rozmajzl@gmail.com>
2017-11-05 15:25:13 -0500

    extraneous semicolon

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
       template: './src/index.html'
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV);
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
 };
