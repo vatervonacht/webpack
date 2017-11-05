2133d07a1e17b70beda8467fe12efe7323da3445
Tim Rozmajzl <tim.rozmajzl@gmail.com>
2017-11-05 15:22:19 -0500

    fixed deploy line

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,9 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV);
     })
   ]
 };
